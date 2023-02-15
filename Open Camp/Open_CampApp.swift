86d0a06e1e20651044de5b6524270b0da61affb5
nickherzig <nickherzig1@gmail.com>
2023-02-14 17:31:14 -0800

    Initial Commit

---
+++ Open Camp/Open_CampApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_CampApp.swift
+//  Open Camp
+//
+//  Created by Nick Herzig on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Open_CampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
