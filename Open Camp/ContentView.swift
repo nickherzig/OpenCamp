42763a045514ff83abb8cc7e9c82d4c8a93c8a80
nickherzig <nickherzig1@gmail.com>
2023-02-15 14:50:35 -0800

    Working on sign in page

--- Open Camp/ContentView.swift
+++ Open Camp/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack(){
+                Text("Open Camp")
+                    .foregroundColor(.white)
+                    .font(.system(size:40, weight: .bold))
+                    .offset(y: -100)
+                Text("Email")
+                    .frame(width: 350, alignment: .leading)
+                    .foregroundColor(.white)
+                    .font(.system(size:20))
+                TextField("", text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Text("Password")
+                    .frame(width: 350, alignment: .leading)
+                    .foregroundColor(.white)
+                    .font(.system(size:20))
+                SecureField("", text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button {
+                    //sign up
+                } label: {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        
+                }
+
+            }
+            .frame(width: 350)
+            
         }
-        .padding()
     }
 }
 
